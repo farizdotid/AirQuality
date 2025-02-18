1095c806ed58c8a6d2d4a03dac3294894d4a90c8
farizvodjo <contact.farizramadhan@gmail.com>
2025-02-18 22:53:55 +0700

    feat: Function get data AQI

--- app/controllers/air_quality_controller.rb
+++ app/controllers/air_quality_controller.rb
@@ -2,23 +2,39 @@ class AirQualityController < ApplicationController
   require "httparty"
 
   def index
-    @loading = true
+    respond_to do |format|
+      format.html
+      format.turbo_stream do
+        url = "#{Rails.application.credentials.dig(:air_quality_api, :base_url)}/api/v1/getCityRankByCitySlug"
+        headers = {
+          "Authorization" => "bearer #{Rails.application.credentials.dig(:air_quality_api, :bearer_token)}",
+          "cityslug" => "indonesia/jawa-barat/bandung",
+          "sensorname" => "aqi"
+        }
+        response = HTTParty.get(url, headers: headers)
+        if response.success?
+          @data = response.parsed_response["data"]
+          @aqius = @data["location"]["aqius"]
+          if @aqius <= 50
+            @bg_color = "bg-green-400"
+          elsif @aqius >= 51 && @aqius <= 100
+            @bg_color = "bg-yellow-400"
+          elsif @aqius >= 101 && @aqius <= 150
+            @bg_color = "bg-orange-400"
+          elsif @aqius >= 151 && @aqius <= 200
+            @bg_color = "bg-red-400"
+          elsif @aqius >= 201 && @aqius <= 300
+            @bg_color = "bg-purple-400"
+          elsif @aqius >= 301 && @aqius <= 500
+            @bg_color = "bg-rose-700"
+          end
+        else
+          @error = "Failed to fetch air quality data."
+        end
 
-    url = "#{Rails.application.credentials.dig(:air_quality_api, :base_url)}/api/v1/getCityRankByCitySlug"
-    headers = {
-      "Authorization" => "bearer #{Rails.application.credentials.dig(:air_quality_api, :bearer_token)}",
-      "cityslug" => "indonesia/jawa-barat/bandung",
-      "sensorname" => "aqi"
-    }
-
-    response = HTTParty.get(url, headers: headers)
-
-    if response.success?
-      @data = response.parsed_response["data"]
-    else
-      @error = "Failed to fetch air quality data."
+        render turbo_stream: turbo_stream.replace("air_quality_data", partial: "air_quality/data", locals: {
+          data: @data, bgColor: @bg_color, error: @error })
+      end
     end
-
-    @loading = false
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root "air_quality#index"
+  get "air_quality", to: "air_quality#index"
 end

83e75a1a44d05ee2795be96e60c305726060abfb
farizvodjo <contact.farizramadhan@gmail.com>
2025-02-19 08:23:16 +0700

    feat: Adding data status

--- app/controllers/air_quality_controller.rb
+++ app/controllers/air_quality_controller.rb
@@ -15,7 +15,7 @@ class AirQualityController < ApplicationController
         if response.success?
           @data = response.parsed_response["data"]
           @aqius = @data["location"]["aqius"]
-          aqi_levels = [
+          @aqi_levels = [
             { range: 0..50, color: "bg-green-400", status: "Good", index: "0 to 50", description: "Air quality is satisfactory, and air pollution poses little or no risk." },
             { range: 51..100, color: "bg-yellow-400", status: "Moderate", index: "51 to 100", description: "Air quality is acceptable. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution." },
             { range: 101..150, color: "bg-orange-400", status: "Unhealthy for Sensitive Groups", index: "101 to 150", description: "Members of sensitive groups may experience health effects. The general public is less likely to be affected." },
@@ -23,7 +23,7 @@ class AirQualityController < ApplicationController
             { range: 201..300, color: "bg-purple-400", status: "Very Unhealthy", index: "201 to 300", description: "Health alert: The risk of health effects is increased for everyone." },
             { range: 301..500, color: "bg-rose-700", status: "Hazardous", index: "301 and higher", description: "Health warning of emergency conditions: everyone is more likely to be affected." }
           ]
-          selected_level = aqi_levels.find { |level| level[:range].include?(@aqius) } || aqi_levels.last
+          selected_level = @aqi_levels.find { |level| level[:range].include?(@aqius) } || @aqi_levels.last
           @bg_color = selected_level[:color]
           @level_status = selected_level[:status]
           @value_of_index = selected_level[:index]
@@ -38,6 +38,7 @@ class AirQualityController < ApplicationController
           levelStatus: @level_status,
           valueOfIndex: @value_of_index,
           description: @description,
+          aqiLevels: @aqi_levels,
           error: @error
         })
       end
